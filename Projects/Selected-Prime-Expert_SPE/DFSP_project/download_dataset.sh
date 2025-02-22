53417c54361473b197f2908faefe45485c1ce957
aaronwu901225main <aaronwu901225main@gmail.com>
2025-02-22 22:02:46 +0800

    Update download_dataset.sh

--- Projects/Selected-Prime-Expert_SPE/DFSP_project/download_dataset.sh
+++ Projects/Selected-Prime-Expert_SPE/DFSP_project/download_dataset.sh
@@ -1,3 +1,3 @@
-wget https://huggingface.co/datasets/cccxi/SPE_project_study/resolve/main/dataset.zip
+wget https://huggingface.co/datasets/AaronWu901225/MIT_states_UT_zappos/resolve/main/dataset.zip
 unzip dataset.zip
 rm dataset.zip
